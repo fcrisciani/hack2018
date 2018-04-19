fb8349df7ec6510d8f0a299a34145a487584f09f
Chris Telfer <ctelfer@docker.com>
2018-04-19 18:12:29 -0400

    Make funnel program translate to old JSON form
    Signed-off-by: Chris Telfer <ctelfer@docker.com>

--- logstasher/funnel.go
+++ logstasher/funnel.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 	"sync"
 	"time"
 )
@@ -34,6 +35,43 @@ func flusher() {
 	}
 }
 
+// XXX This is hacky to say the least
+// And I liked the untranslated field names better :)
+// Fields left to munge:  timestamp, ICMP fields
+func translate(s string) string {
+	var trans = []struct {
+		old, new string
+	}{
+		{"host", "dvc"},
+		{"state", "ct.event"},
+		{"orig_src", "src_ip"},
+		{"orig_dst", "dest_ip"},
+		{"orig_proto", "orig.ip.protocol"},
+		{"orig_spt", "orig.l4.sport"},
+		{"orig_dpt", "orig.l4.dport"},
+		{"orig_pkts", "orig.raw.pktcount"},
+		{"orig_bytes", "orig.raw.pktlen"},
+		{"reply_src", "reply.ip.saddr.str"},
+		{"reply_dst", "reply.ip.daddr.str"},
+		{"reply_proto", "reply.ip.protocol"},
+		{"reply_spt", "reply.l4.sport"},
+		{"reply_dpt", "reply.l4.dport"},
+		{"reply_pkts", "reply.raw.pktcount"},
+		{"reply_bytes", "reply.raw.pktlen"},
+		{"\"tcp\"", "6"},
+		{"\"udp\"", "17"},
+		{"\"icmp\"", "11"},
+		{"\"NEW\"", "1"},
+		{"\"DEL\"", "4"},
+	}
+
+	for i := range trans {
+		s = strings.Replace(s, trans[i].old, trans[i].new, 1)
+	}
+
+	return s
+}
+
 func handleConn(c net.Conn) {
 	log.Printf("New connection from %s\n", c.RemoteAddr())
 
@@ -42,7 +80,7 @@ func handleConn(c net.Conn) {
 
 	for s.Scan() {
 		lock.Lock()
-		_, err := writer.WriteString(s.Text() + "\n")
+		_, err := writer.WriteString(translate(s.Text() + "\n"))
 		lock.Unlock()
 		if err != nil {
 			log.Printf("Error reading from remote endpoint %s: %v\n", c.RemoteAddr(), err)
