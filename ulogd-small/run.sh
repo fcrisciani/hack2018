999e11a7c9aaee61ed437ae09aa882955345070a
Chris Telfer <ctelfer@docker.com>
2018-04-19 18:09:38 -0400

    Update to RFC3339 timestamps
    Also make able to discover server (for testing)
    
    Signed-off-by: Chris Telfer <ctelfer@docker.com>

--- ulogd-small/run.sh
+++ ulogd-small/run.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-#ESADDR=`ip route | grep default | awk '{for(i=1;i<=NF;i++){if($i=="via"){print $(i+1)}}}'`
-#if [ "$ESADDR" = "" ] ; then
-#	echo "Could not find elastic search address" >&2
-#	exit 1
-#fi
-
-# Temprorary
-ESADDR=52.42.55.249
+if [ $# -gt 0 ] ; then
+	ESADDR=`ip route | grep default | awk '{for(i=1;i<=NF;i++){if($i=="via"){print $(i+1)}}}'`
+	if [ "$ESADDR" = "" ] ; then
+		echo "Could not find elastic search address" >&2
+		exit 1
+	fi
+else
+	# Temprorary
+	ESADDR=52.42.55.249
+fi
 
 ulogd -d
 while [ ! -f /var/log/ulogd_flow_events.log ] ; do sleep 1 ; done
