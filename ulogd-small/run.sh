95538bb50502e2ef50269b750e1de064ebff4d13
Chris Telfer <ctelfer@docker.com>
2018-04-19 17:20:40 -0400

    Update to work in our test environment
    Also update the AWK script to generate a timestamp in a more
    universal format.
    
    Signed-off-by: Chris Telfer <ctelfer@docker.com>

--- ulogd-small/run.sh
+++ ulogd-small/run.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-ESADDR=`ip route | grep default | awk '{for(i=1;i<=NF;i++){if($i=="via"){print $(i+1)}}}'`
-if [ "$ESADDR" = "" ] ; then
-	echo "Could not find elastic search address" >&2 
-	exit 1
-fi
+
+#ESADDR=`ip route | grep default | awk '{for(i=1;i<=NF;i++){if($i=="via"){print $(i+1)}}}'`
+#if [ "$ESADDR" = "" ] ; then
+#	echo "Could not find elastic search address" >&2
+#	exit 1
+#fi
+
+# Temprorary
+ESADDR=52.42.55.249
 
 ulogd -d
-while [ ! -f /var/log/ulogd_flow_events.log ] ; do sleep 1 ; done 
-tail -f /var/log/ulogd_flow_events.log | /usr/bin/ulogd2json | nc $ESADDR 9000
+while [ ! -f /var/log/ulogd_flow_events.log ] ; do sleep 1 ; done
+tail -f /var/log/ulogd_flow_events.log | /usr/bin/ulogd2json | nc -v $ESADDR 55555
